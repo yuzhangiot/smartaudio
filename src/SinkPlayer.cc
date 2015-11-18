4b096f2e858e01d926b74e5bd1bb2febe027c0ab
yuzhangiot <yuzhangiot@gmail.com>
2015-11-18 20:53:56 +0800

    time syn thread

--- src/SinkPlayer.cc
+++ src/SinkPlayer.cc
@@ -74,8 +74,6 @@ class SignallingObject : public BusObject {
   private:
     const InterfaceDescription::Member* mAudioDataMember;
 
-    ajn::BusAttachment* mMsgBus;
-
     QStatus Get(const char* ifcName, const char* propName, MsgArg& val) {
         QStatus status = ER_OK;
 
