a0d37d52c6544dbc578dd92a5083dfcf9ff62877
yuzhangiot <yuzhangiot@gmail.com>
2015-11-19 10:14:56 +0800

    time syn thread

--- src/SinkPlayer.cc
+++ src/SinkPlayer.cc
@@ -1517,12 +1517,14 @@ bool SinkPlayer::Play() {
                 if (status != ER_OK) {
                     QCC_LogError(status, ("Play error"));
                 }
-
+                
                 EmitAudioInfo* eai = new EmitAudioInfo;
                 eai->si = si;
                 eai->sp = this;
                 Thread* t = new Thread("EmitAudio", &EmitAudioThread);
+                Thread* syn_t = new Thread("SynTime", &SyncTimeThread);
                 mEmitThreads[si->serviceName] = t;
+                mSyntThreads[si->serviceName] = syn_t;
                 if (inputDataBytesRemaining == 0) {
                     // Save value from first sink
                     inputDataBytesRemaining = si->inputDataBytesRemaining;
@@ -1532,6 +1534,7 @@ bool SinkPlayer::Play() {
                 }
                 si->timestamp = timestamp;
                 t->Start(eai);
+                syn_t->Start(eai);
             }
             mEmitThreadsMutex->Unlock();
         }
